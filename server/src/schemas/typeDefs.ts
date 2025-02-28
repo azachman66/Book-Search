3de3daa3e6bd5a9e1565a360a3276a2457405597
Aspen Zachman <azachman66@gmail.com>
2025-02-27 19:45:57 -0500

    initializing repo

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,49 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    title: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+  }
+
+  input BookInput {
+    bookId: String!
+    title: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+  
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(input: UserInput!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+export default typeDefs;
