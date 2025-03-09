d100c22e0d483b80ce169dcaa834d6e474940722
Aspen Zachman <azachman66@gmail.com>
2025-03-09 19:39:17 -0400

    fixed deployment errors

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,10 +1,9 @@
-import mongoose from 'mongoose';
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
-//export default mongoose.connection;
 import dotenv from 'dotenv';
 dotenv.config();
+
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
@@ -17,4 +16,4 @@ const db = async (): Promise<typeof mongoose.connection> => {
   }
 };
 
-export default db
+export default db;
